b5d79741eab9b2611c241594710d9f367b7f56e1
Michael Haberler <git@mah.priv.at>
2023-08-27 17:41:28 +0200

    adapt for gdal 3.6 and 3.7
    3.6.2 is current for debian bookworm
    3.7.1_1 is current for MacOS brew

--- src/GDALTiler.cpp
+++ src/GDALTiler.cpp
@@ -216,10 +216,10 @@ GDALTiler::createRasterTile(GDALDataset *dataset, const TileCoordinate &coord) c
  * This code is adapted from that found in `gdalwarp.cpp` implementing the
  * `gdalwarp -ovr` option.
  */
-#if ( GDAL_VERSION_MAJOR >= 3 )
-#include "myods.cpp"
-#elif ( GDAL_VERSION_MAJOR >= 2 && GDAL_VERSION_MINOR >= 2 )
-#include "gdaloverviewdataset-gdal2x.cpp"
+#if ( GDAL_VERSION_MAJOR == 3 && GDAL_VERSION_MINOR >= 7 )
+#include "gdaloverviewdataset-gdal3_7.cpp"
+#elif ( GDAL_VERSION_MAJOR == 3 && GDAL_VERSION_MINOR >= 6 )
+#include "gdaloverviewdataset-gdal3_6.cpp"
 #endif
 
 static
